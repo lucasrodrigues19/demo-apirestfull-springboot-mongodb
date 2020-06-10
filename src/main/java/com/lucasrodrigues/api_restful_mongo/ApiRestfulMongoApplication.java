880f6454d0338ff42871d871a0c65fc79e09fdef
lucasrodrigues19 <pereirarodriguesl194@gmail.com>
2020-06-10 16:22:16 -0300

    Primeiro commmit

---
+++ src/main/java/com/lucasrodrigues/api_restful_mongo/ApiRestfulMongoApplication.java
@@ -0,0 +1,13 @@
+package com.lucasrodrigues.api_restful_mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestfulMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestfulMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucasrodrigues/api_restful_mongo/ApiRestfulMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasrodrigues.api_restful_mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestfulMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
