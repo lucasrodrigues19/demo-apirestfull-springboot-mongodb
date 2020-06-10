b3c0fe48b72525b3ead99d52c529eb41b257d832
lucasrodrigues19 <pereirarodriguesl194@gmail.com>
2020-06-10 20:15:46 -0300

    Criando entity Post com User aninhado

--- src/main/java/com/lucasrodrigues/api_restful_mongo/config/Instantiation.java
+++ src/main/java/com/lucasrodrigues/api_restful_mongo/config/Instantiation.java
@@ -1,30 +1,46 @@
 package com.lucasrodrigues.api_restful_mongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.lucasrodrigues.api_restful_mongo.domain.Post;
 import com.lucasrodrigues.api_restful_mongo.domain.User;
+import com.lucasrodrigues.api_restful_mongo.repository.PostRepository;
 import com.lucasrodrigues.api_restful_mongo.repository.UserRepository;
 
 @Configuration
 public class Instantiation implements CommandLineRunner{
 
-private UserRepository repo;
+private UserRepository userRepo;
+private PostRepository postRepo;
 	
 	@Autowired
-	public Instantiation(UserRepository repo) {
-		this.repo = repo;
+	public Instantiation(UserRepository userRepo,PostRepository postRepo) {
+		this.userRepo = userRepo;
+		this.postRepo = postRepo;
 	}
 	@Override
 	public void run(String... args) throws Exception {
-		repo.deleteAll();
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
+		userRepo.deleteAll();
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
-		repo.saveAll(Arrays.asList(maria,alex,bob));
+		userRepo.saveAll(Arrays.asList(maria,alex,bob));
+		
+		
+		postRepo.deleteAll();
+		Post p1 = new Post(null,sdf.parse("21/03/2018"),"Partiu viagem","Vou viajar para São Paulo. Abraços!",maria);
+		Post p2 = new Post(null,sdf.parse("23/03/2018"),"Bom dia","Acordei feliz hoje!",maria);
+		postRepo.saveAll(Arrays.asList(p1,p2));
 	}
 
 }
---
+++ src/main/java/com/lucasrodrigues/api_restful_mongo/repository/PostRepository.java
@@ -0,0 +1,12 @@
+package com.lucasrodrigues.api_restful_mongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.lucasrodrigues.api_restful_mongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+
+	
+}
