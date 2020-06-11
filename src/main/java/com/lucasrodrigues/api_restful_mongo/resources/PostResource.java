6512e23b46199329bd9724ed1c2a8ac1713b14cd
lucasrodrigues19 <pereirarodriguesl194@gmail.com>
2020-06-10 23:10:41 -0300

    Obtendo um post por id

---
+++ src/main/java/com/lucasrodrigues/api_restful_mongo/resources/PostResource.java
@@ -0,0 +1,33 @@
+package com.lucasrodrigues.api_restful_mongo.resources;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lucasrodrigues.api_restful_mongo.domain.Post;
+import com.lucasrodrigues.api_restful_mongo.services.PostService;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+	private PostService service;
+
+	@Autowired
+	public PostResource(PostService service) {
+		this.service = service;
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Post> findById(@PathVariable String id) {
+		Post post = service.findById(id);
+		return ResponseEntity.ok().body(post);
+	}
+
+}
---
+++ src/main/java/com/lucasrodrigues/api_restful_mongo/services/PostService.java
@@ -0,0 +1,28 @@
+package com.lucasrodrigues.api_restful_mongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lucasrodrigues.api_restful_mongo.domain.Post;
+import com.lucasrodrigues.api_restful_mongo.repository.PostRepository;
+
+@Service
+public class PostService {
+
+	private PostRepository repo;
+
+	@Autowired
+	public PostService(PostRepository repo) {
+		this.repo = repo;
+	}
+
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+	
+}
