3a7524271f4534d0772ad247c188829912e31a27
lucasrodrigues19 <pereirarodriguesl194@gmail.com>
2020-06-11 00:06:09 -0300

    fazendo consultas com query methods

--- src/main/java/com/lucasrodrigues/api_restful_mongo/resources/PostResource.java
+++ src/main/java/com/lucasrodrigues/api_restful_mongo/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.lucasrodrigues.api_restful_mongo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lucasrodrigues.api_restful_mongo.domain.Post;
+import com.lucasrodrigues.api_restful_mongo.resources.util.URL;
 import com.lucasrodrigues.api_restful_mongo.services.PostService;
 
 @RestController
@@ -25,6 +29,13 @@ public class PostResource {
 	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post post = service.findById(id);
 		return ResponseEntity.ok().body(post);
+	}
+	
+	@RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {
+		text = URL.decodeParam(text);
+		List<Post> posts = service.findByTitle(text);
+		return ResponseEntity.ok().body(posts);
 	}
 
 }
--- src/main/java/com/lucasrodrigues/api_restful_mongo/services/PostService.java
+++ src/main/java/com/lucasrodrigues/api_restful_mongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.lucasrodrigues.api_restful_mongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,5 +25,8 @@ public class PostService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
+	public List<Post> findByTitle(String text){
+		return repo.findByTitleContainingIgnoreCase(text);
+	}
 	
 }
